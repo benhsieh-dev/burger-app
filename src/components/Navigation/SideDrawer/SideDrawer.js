7f37460d568523e7eaf660577016e954d568782f
Kevin Hsieh <bqh5026@Kevins-iMac.local>
2019-06-08 22:38:28 -0400

    Improve app performance and update folder structure

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import sideDrawerModule from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux'; 
+import Aux from '../../../hoc/Aux/Aux'; 
 
 const sideDrawer = (props) => {
     let attachedClasses = [sideDrawerModule.SideDrawer, sideDrawerModule.Close];
